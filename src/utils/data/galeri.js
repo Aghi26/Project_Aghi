207803512302406065a3081676dd0e4570c3c944
Aghi <aghisaha@gmail.com>
2025-10-06 12:14:59 +0700

    galery

--- src/utils/data/galeri.js
+++ src/utils/data/galeri.js
@@ -2,7 +2,7 @@ const galeriImages = [
   { id: 1, url: 'assets/bnw.jpg', alt: 'Landscape 1' },
   { id: 2, url: 'assets/berita6.jpg', alt: 'Portrait 1' },
   { id: 3, url: 'https://placehold.co/800x500', alt: 'Landscape 2' },
-  { id: 4, url: 'https://placehold.co/500x700', alt: 'Portrait 2' },
+  { id: 4, url: 'assets/rf.jpg', alt: 'Portrait 2' },
   { id: 5, url: 'https://placehold.co/700x400', alt: 'Landscape 3' },
   { id: 6, url: 'https://placehold.co/500x500', alt: 'Square' }
 ]
