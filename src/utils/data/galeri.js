dc102d250a1c252e74cfa0ce616b44be10798d1c
Aghi <aghisaha@gmail.com>
2025-10-06 19:08:08 +0700

    NEW

--- src/utils/data/galeri.js
+++ src/utils/data/galeri.js
@@ -5,7 +5,6 @@ const galeriImages = [
   { id: 4, url: 'assets/rf.jpg', alt: 'Rizky Febian' },
   { id: 5, url: 'assets/hindia.jpg', alt: 'Hindia Concert' },
   { id: 6, url: 'assets/ketawa.jpg', alt: 'Open Mic Ketawa Comedy Club' }
-  { id: 7, url: 'assets/aaa.jpg', alt: 'Reality Club 212' }
 ]
 
 export default galeriImages
