8109b9e5198621cc3a14463b3293afa74fb35a11
Aghi <aghisaha@gmail.com>
2025-10-06 13:45:42 +0700

    galery

--- src/utils/data/galeri.js
+++ src/utils/data/galeri.js
@@ -1,10 +1,11 @@
 const galeriImages = [
   { id: 1, url: 'assets/bnw.jpg', alt: 'FULLHOUSE JKT48' },
-  { id: 2, url: 'assets/berita6.jpg', alt: 'Reality Club 212' },
+  { id: 2, url: 'assets/berita6.jpg', alt: 'Antonym' },
   { id: 3, url: 'assets/nadin.jpg', alt: 'Nadin Amizah' },
   { id: 4, url: 'assets/rf.jpg', alt: 'Rizky Febian' },
   { id: 5, url: 'assets/hindia.jpg', alt: 'Hindia Concert' },
   { id: 6, url: 'assets/ketawa.jpg', alt: 'Open Mic Ketawa Comedy Club' }
+  { id: 7, url: 'assets/aaa.jpg', alt: 'Reality Club 212' }
 ]
 
 export default galeriImages
