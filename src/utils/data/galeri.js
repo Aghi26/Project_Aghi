929c994086718488fc78195df232519a740448a4
Aghi <aghisaha@gmail.com>
2025-10-06 11:38:04 +0700

    galery

--- src/utils/data/galeri.js
+++ src/utils/data/galeri.js
@@ -1,6 +1,6 @@
 const galeriImages = [
   { id: 1, url: '/assets/bnw.jpg/dll', alt: 'Landscape 1' },
-  { id: 2, url: 'https://placehold.co/400x600', alt: 'Portrait 1' },
+  { id: 2, url: 'https://www.tagar.id/Asset/uploads2019/1575038520042-baskara-putra-alias-hindia.jpg', alt: 'Portrait 1' },
   { id: 3, url: 'https://placehold.co/800x500', alt: 'Landscape 2' },
   { id: 4, url: 'https://placehold.co/500x700', alt: 'Portrait 2' },
   { id: 5, url: 'https://placehold.co/700x400', alt: 'Landscape 3' },
